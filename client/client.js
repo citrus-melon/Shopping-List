6f4d598633d28f6e2662c38cbf8fe91d72710a77
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-04-03 16:57:27 -0400

    Simplify focusNextOrNew() to focusOrNew()

--- client/client.js
+++ client/client.js
@@ -16,22 +16,19 @@ const shoppingListApp = {
       this.items.splice(index, 1);
     },
     focusItem (index) {
+      this.editing = true;
       this.$nextTick(() => {
         this.itemRefs[index].focus();
       });
     },
-    focusNextOrNew (currentIndex) {
-      let nextIndex = this.items.length;
-      if (currentIndex !== undefined) nextIndex = currentIndex + 1;
-      if (this.items[nextIndex] === undefined) {
+    focusOrNew (index) {
+      if (this.items[index] === undefined) {
         this.items.push({id: this.nextId++, text: ''});
       }
-      this.editing = true;
-      this.focusItem(nextIndex);
+      this.focusItem(index);
     },
     editBtnClick () {
-      this.editing = true;
-      this.focusItem(this.items.length - 1);
+      if(!this.editing) this.focusOrNew(0);
     },
     setItemRef(el) {
       if (el) {
@@ -49,20 +46,15 @@ const app = Vue.createApp(shoppingListApp)
 app.component('todo-item', {
   props: ['todo', 'editing', 'last'],
   emits: ['remove', 'next'],
-  template: `<li><span>{{ todo.text }}<input type="text" ref="input" v-if="editing" v-model="todo.text" @keydown.tab="handleTab" @keyup.enter="handleEnter" @blur="handleBlur"/></span><span v-if="!last">, </span></li>`,
+  template: `<li><span>{{ todo.text }}<input type="text" ref="input" v-if="editing" v-model="todo.text" @keydown.tab.prevent="handleNext" @keyup.enter="handleNext" @blur="handleBlur"/></span><span v-if="!last">, </span></li>`,
   methods: {
     handleBlur() {
       if(this.todo.text == '') {
         this.$emit('remove');
       }
     },
-    handleEnter() {
+    handleNext() {
       this.$emit('next');
-    },
-    handleTab() {
-      if (this.last) {
-        this.$emit('next');
-      }
     },
     focus() {
       this.$nextTick(() => {
